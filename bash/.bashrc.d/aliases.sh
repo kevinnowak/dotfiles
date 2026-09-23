022feabfd909aecdf4250cc7f43bebb3e6f71330
Kevin Nowak <kevin.nowak@posteo.de>
2026-09-23 22:06:22 +0200

    Initial commit

---
+++ bash/.bashrc.d/aliases.sh
@@ -0,0 +1,11 @@
+# Package management
+alias sdu="sudo dnf update"
+alias sda="sudo dnf autoremove"
+
+# File system
+alias la="ll -A"
+
+# Git
+alias gst="git status"
+
+
---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,11 @@
+;; Backup files
+(setq backup-directory-alist
+      '(("." . "~/.local/state/emacs/backups")))
+
+;; Line numbers
+(global-display-line-numbers-mode 1)
+
+;; Intendation
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+
